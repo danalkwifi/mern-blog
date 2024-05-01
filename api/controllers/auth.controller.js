e83575a2d4ef52d350dc00845afe7eecd04f8406
dalkwifi <dalkwifi@uwo.ca>
2024-04-30 21:21:41 -0400

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypts from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 //next to use the middleware
 export const signup = async(req, res, next) => {
@@ -26,11 +27,35 @@ export const signup = async(req, res, next) => {
     next(error);
 }
 
-
-
   
+};
 
+export const sigin = async(req, res, next) => {
+  const {email, password} = req.body;
+
+  if(!email || !password || email === '' || password === ''){
+    next(errorHandler(400, 'All fileds are required'));
+  }
+
+  try{
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcrypts.compareSync(password, validUser.password);
+    
+    if(!validPassword){
+      return next(errorHandler(400, 'Invalid password'));
+    }
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET); //secret key
+
+    const {password: pass, ...rest} = validUser._doc;
+
+    res.status(200).cookie('access_token', token, {
+      httpOnly: true}).json(rest);
   
-  
+  }catch(error){
+    next(error);
+  }
 };
 
