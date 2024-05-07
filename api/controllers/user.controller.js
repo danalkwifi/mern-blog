994c5d02f2b4ac5e5b4b818dbec856c18b6a19a7
dalkwifi <dalkwifi@uwo.ca>
2024-05-07 16:34:40 -0400

    complete delete user account functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -55,13 +55,13 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.userId){
-        return next(errorHandler(403, 'You are not allowed to delete'));
+    if (req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to delete this user'));
     }
-    try{
-        await User.findByIdAndDelete(req.params.userId);
-        res.status(200).json('User has been deleted');
-    }catch(error){
-
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted');
+    } catch (error) {
+      next(error);
     }
-}
+  };
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput, Modal } from "flowbite-react";
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, uploadBytesResumable, ref } from 'firebase/storage';
@@ -7,13 +7,14 @@ import { app } from "../firebase";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-import{updateStart, updateSuccess, updateFailure} from '../redux/user/userSlice.js';
+import{updateStart, updateSuccess, updateFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice.js';
 import { useDispatch } from "react-redux";
 import { updateUser } from "../../../api/controllers/user.controller.js";
+import {HiOutlineExclamationCircle} from 'react-icons/hi';
 
 
 export default function DashProfile() {
-    const {currentUser} = useSelector((state) => state.user); // Assuming state.user has a property currentUser
+    const {currentUser, error} = useSelector((state) => state.user); // Assuming state.user has a property currentUser
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -21,6 +22,9 @@ export default function DashProfile() {
     const [imageFileUploading, setImageFileUploading] = useState(false);
     const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
     const [updateUserError, setUpdateUserError] = useState(null);
+
+    const [showModal, setShowModal] = useState(false);
+
     const [formData, setFormData] = useState({});
     const filePickerRef = useRef();
     const dispatch = useDispatch();
@@ -127,6 +131,24 @@ export default function DashProfile() {
         }
     };
 
+    const handleDeleteUser = async () => {
+        setShowModal(false);
+        try {
+          dispatch(deleteUserStart());
+          const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+            method: 'DELETE',
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            dispatch(deleteUserFailure(data.message));
+          } else {
+            dispatch(deleteUserSuccess(data));
+          }
+        } catch (error) {
+          dispatch(deleteUserFailure(error.message));
+        }
+    };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
         <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -171,7 +193,7 @@ export default function DashProfile() {
             </Button>
         </form>
         <div className="text-red-500 flex justify-between mt-5">
-            <span className="cursor-pointer">Delete Account</span>
+            <span onClick={()=> setShowModal(true)} className="cursor-pointer">Delete Account</span>
             <span className="cursor-pointer">Sign Out</span>
         </div>
         {updateUserSuccess && (
@@ -184,6 +206,24 @@ export default function DashProfile() {
                 {updateUserError}
             </Alert>
         )}
+        {error && (
+            <Alert color='failure' className="mt-5"> 
+                {error}
+            </Alert>
+        )}
+        <Modal show={showModal} onClose={() => setShowModal(flase)} popup size='md'>
+            <Modal.Header/>
+            <Modal.Body>
+                <div className="text-center">
+                    <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                    <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your account?</h3>
+                    <div className="flex justify-center gap-4">
+                        <Button color='failure' onClick={handleDeleteUser}>Yes, I'm sure</Button>
+                        <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
     </div>
   );
 }
