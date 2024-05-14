fd2574b4106015f08a8124de6f6303521da82a94
dalkwifi <dalkwifi@uwo.ca>
2024-05-14 16:55:22 -0400

    add delete post funtionality to the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
-import { set } from 'mongoose';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
@@ -105,7 +104,10 @@ export default function DashPosts() {
                 <Table.Cell>{post.category}</Table.Cell>
 
                 <Table.Cell>
-                    <span className='font-medium text-red-500 hover:underline'>Delete</span>
+                    <span onClick={() => {
+                      setShowModal(true);
+                      setPostIdToDelete(post._id);
+                    }} className='font-medium text-red-500 hover:underline'>Delete</span>
                   </Table.Cell>
                 <Table.Cell>
                     <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
@@ -118,12 +120,32 @@ export default function DashPosts() {
             ))}
             
           </Table>
+
+          {
+            showMore && (
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                Show more
+              </button>
+            )
+          }
         
         </>
       ) : (
         <p>You have no posts yet!</p>
       )}
-      
+      <Modal show={showModal} onClose={() => setShowModal(flase)} popup size='md'>
+            <Modal.Header/>
+            <Modal.Body>
+                <div className="text-center">
+                    <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                    <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete this post?</h3>
+                    <div className="flex justify-center gap-4">
+                        <Button color='failure' onClick={handleDeletePost}>Yes, I'm sure</Button>
+                        <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>      
     </div>
   );
 }
