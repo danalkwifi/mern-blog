04ba64f1ee468730256f4c2daba6d6fa7f42b6d8
dalkwifi <dalkwifi@uwo.ca>
2024-05-17 12:46:16 -0400

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,13 @@ import OnlyAdmin from "./components/OnlyAdmin.jsx";
 import CreatePost from "./pages/CreatePost.jsx";
 import UpdatePost from "./pages/UpdatePost.jsx";
 import PostPage from "./pages/PostPage.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
